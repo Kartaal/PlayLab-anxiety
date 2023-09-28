7a480032ecd0d7d703fda9436a86824ac08ec14f
Niclas Valentiner <niclasv@hotmail.com>
2023-09-28 14:29:45 +0200

    Added task creation
    Touch ghost task to create a new task.

--- PlayLab-anxiety/Assets/Scripts/TaskManager.cs
+++ PlayLab-anxiety/Assets/Scripts/TaskManager.cs
@@ -1,11 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TaskManager : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject taskPrefab;
+    [SerializeField] private GameObject ghostTask;
+
+    public void CreateNewTask()
+    {
+        int newTaskIndex = ghostTask.transform.GetSiblingIndex();
+        
+        GameObject newTask = Instantiate(taskPrefab, transform);
+        newTask.transform.SetSiblingIndex(newTaskIndex);
+    }
 }
